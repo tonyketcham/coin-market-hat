d12bc928c1dc116bd0336c32b421ff566fdd01c7
tonyketcham <tony@karmalies.studio>
2022-01-08 00:13:22 -0600

    feat: coin typing

---
+++ types.ts
@@ -0,0 +1,27 @@
+export interface Coin {
+  id: string;
+  name: string;
+  symbol: string;
+  image: string;
+  current_price: number;
+  market_cap: number;
+  market_cap_rank: number;
+  total_volume: number;
+  high_24h: number;
+  low_24h: number;
+  price_change_24h: number;
+  price_change_percentage_24h: number;
+  market_cap_change_24h: number;
+  market_cap_change_percentage_24h: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  ath: number;
+  ath_change_percentage: number;
+  ath_date: string;
+  atl: number;
+  atl_change_percentage: number;
+  atl_date: string;
+  roi: null;
+  last_updated: string;
+}
